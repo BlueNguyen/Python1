079b3edc789244ae2bdd9acf1ddf6d578d6e4993
BlueNguyen <143895447+BlueNguyen@users.noreply.github.com>
2024-06-01 09:20:15 +0700

    modify bigO.py

---
+++ python3/bigO.py
@@ -0,0 +1,37 @@
+#***BÀI TẬP 1: Xác định Big O notation
+i = 0             #=> O(1)  --> 1
+while (i < n):    #=> O(n) - vòng lặp này sẽ chạy n lần --> n
+    i = i + 1     #=> O(1) - câu lệnh này được thực hiện n lần trong vòng lặp --> n
+
+#---> F(n)= 1+2n
+#---> O(n)
+
+
+#***BÀI TẬP 2: Xác định Big O notation
+i = 0               #=> O(1)  --> 1
+while (i < n):      #=> O(n) - vòng lặp này sẽ chạy n/100 lần --> n/100
+    i = i + 100     #=> O(1) - câu lệnh này được thực hiện n/100 lần trong vòng lặp --> n/100
+
+#---> F(n)= 1+2n/100
+#---> O(n)
+
+
+#***BÀI TẬP 3: Xác định Big O notation
+while (i < n):          # O(n) do i tăng lên 100 mỗi lần, nên chạy n/100 lần
+    while (a < n):      # O(n^2) cho vòng lặp này
+        while (b < n):  # O(n) cho vòng lặp này
+            b = b + 1
+        a = a + 1
+    while (c < n):      # O(n) cho vòng lặp này
+        c = c + 1
+    i = i + 100                     
+
+
+#---> F(n)= O(n)*O(n^2)             #(2n/100 + 4n^2/100 + 2n^3/100)
+#---> O(n^3)
+
+
+
+
+
+
