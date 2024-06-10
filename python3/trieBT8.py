18810ba2d99fc3b87b02e0dc1a40e10aa6046c31
BlueNguyen <143895447+BlueNguyen@users.noreply.github.com>
2024-06-10 12:38:58 +0700

    add trieBT8.py

---
+++ python3/trieBT8.py
@@ -0,0 +1,7 @@
+# Mảng products
+products = ["cat", "banana", "obama", "batman", "car", "cow", "alibaba"]
+
+# Duyệt qua mảng và in ra chỉ số của phần tử bắt đầu bằng "ba"
+for i in range(len(products)):
+    if products[i][:2] == "ba":
+        print(f'Phần tử "{products[i]}" có chỉ số {i}')
